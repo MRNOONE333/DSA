bb56926d56ca630fb1a50f6b25ee8d9cf12b9e38
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2024-05-31 12:35:04 +0530

    day 18

--- 1pointers/1_pointers.cpp
+++ 1pointers/1_pointers.cpp
@@ -1,25 +1,4 @@
-//  pointer Airthmetsics
 
-// ------------------------------code1. 
-// #include<iostream>
-// using namespace std;
-
-// int main(){
-//     int i =10;
-//     int*  p = &i;
-//     cout<< p<< endl << *p;
-    
-// }
-
-// output - 
-// 0x61ff08
-// 10
-
-//  if i have to move p by one step
-
-
-// ------------------------------code2.
-// see readme cell 3 imge-1,
 
 #include<iostream>
 using namespace std;
