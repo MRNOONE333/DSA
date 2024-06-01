66ff8c72ed4b7ab5a0e18d3e1098b27cbd4df8ae
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2024-06-01 08:18:37 +0530

    day 18

--- 1pointers/5doublePointerIncrementAddressFunction.cpp
+++ 1pointers/5doublePointerIncrementAddressFunction.cpp
@@ -1,22 +1,53 @@
 #include <iostream>
 using namespace std;
 
-void increment(int **p){
-  (**p)++;
+void incrementAddress(int** x) {
+    (*x) += 1; // Increment the address stored in the pointer
+    // or  
+    // (**x) += 1;
 }
 
 int main() {
+    int i = 10;
+    int* p = &i;
 
-int num = 10;
- int *ptr = &num;
- increment(&ptr);
- cout << num << endl;
-
-
+    cout << "Address stored in p: " << p <<","<< *p<< endl;
+    incrementAddress(&p); // Pass the address of the pointer to the function
+    cout << "Address after increment: " << p << ","<< *p<< endl;
 }
 
-//output
-// 11
+/*
+In the function void incrementAddress(int** x), 
+int** x receives the address of an int* (pointer to an integer). 
+By dereferencing (*x), you access the value stored at the memory address pointed to by x, 
+which happens to be an int*. Then, when you increment this value using (*x) += 1;, 
+you're effectively incrementing the address stored in the pointer, not the content it points to.
+*/
+
+
+//    ...............   CRUX  -  HUI HUI HUI UHI HUI HUI HUI HUI HI UHIU ..............
+// Inside the increment function, the value pointed to by *p (which is ptr) is incremented by 1.
+
+
+// #include <iostream>
+// using namespace std;
+
+// void increment(int **p){
+//   (**p)++;
+// }
+
+// int main() {
+
+// int num = 10;
+//  int *ptr = &num;
+//  increment(&ptr);
+//  cout << num << endl;
+
+
+// }
+
+// //output
+// // 11
 
 
 // #include <iostream>
@@ -36,5 +67,5 @@ int num = 10;
 
 // }
 
-//output
-// 10
+// //output
+// // 10
