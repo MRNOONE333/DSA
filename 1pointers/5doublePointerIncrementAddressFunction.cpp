28c5d27ebad6c08e6dee435c761830e6508dd494
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2024-06-01 08:17:47 +0530

    day 18

---
+++ 1pointers/5doublePointerIncrementAddressFunction.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void increment(int **p){
+  (**p)++;
+}
+
+int main() {
+
+int num = 10;
+ int *ptr = &num;
+ increment(&ptr);
+ cout << num << endl;
+
+
+}
+
+//output
+// 11
+
+
+// #include <iostream>
+// using namespace std;   
+
+// void increment(int **p){
+//   (*p)++;
+// }
+
+// int main() {
+
+// int num = 10;
+//  int *ptr = &num;
+//  increment(&ptr);
+//  cout << num << endl;
+
+
+// }
+
+//output
+// 10
