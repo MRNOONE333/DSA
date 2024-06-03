aafe0899611da48d110691e3e9f27574e9b2b6ce
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2024-06-03 09:05:21 +0530

    day 21

--- 2dynamicMemoryAllocation/2referenceVar.cpp
+++ 2dynamicMemoryAllocation/2referenceVar.cpp
@@ -1,19 +1,41 @@
+// #include <iostream>
+// using namespace std;
+
+// void increment(int x) {   // if we remove & from here it will print 10,10
+//     x += 1; // Increment the referenced value
+// }
+
+// int main() {
+//     int x = 10;
+
+//     cout << "Original value of x: " << x << endl;
+
+//     increment(x); // Pass i by reference
+
+//     cout << "Value of i after increment: " << x << endl;
+
+// }
+// // Original value of x: 10
+// // Value of i after increment: 10
+
+
+
 #include <iostream>
 using namespace std;
 
-void increment(int x) {   // if we remove & from here it will print 10,10
-    x += 1; // Increment the referenced value
+void increment(int& a) {   // if we remove & from here it will print 10,10
+    a += 1; // Increment the referenced value
 }
 
 int main() {
-    int x = 10;
+    int i = 10;
 
-    cout << "Original value of x: " << x << endl;
+    cout << "Original value of i: " << i << endl;
 
-    increment(x); // Pass i by reference
+    increment(i); // Pass i by reference
 
-    cout << "Value of i after increment: " << x << endl;
+    cout << "Value of i after increment: " << i << endl;
 
 }
-// Original value of x: 10
-// Value of i after increment: 10
+// Original value of i: 10
+// Value of i after increment: 11
