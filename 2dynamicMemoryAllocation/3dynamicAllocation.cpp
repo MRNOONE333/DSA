22c928c1eca3370b2a4421bb25602c09bacdb5f1
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2024-06-03 09:06:33 +0530

    day 21

--- 2dynamicMemoryAllocation/3dynamicAllocation.cpp
+++ 2dynamicMemoryAllocation/3dynamicAllocation.cpp
@@ -1,25 +1,25 @@
 // See cell 6.
 
-#include <iostream>
-#include <vector>
+// #include <iostream>
+// #include <vector>
 
-using namespace std;
+// using namespace std;
 
-int main() {
-    int n;
+// int main() {
+//     int n;
 
-    cout << "Input n: ";
-    cin >> n;
+//     cout << "Input n: ";
+//     cin >> n;
 
-    vector<int> arr(n, 0); // Create a vector with 'n' elements, each initialized to 0
+//     vector<int> arr(n, 0); // Create a vector with 'n' elements, each initialized to 0
 
-    // Get the size in bytes
-    size_t sizeInBytes = sizeof(arr[0]) * arr.size();
+//     // Get the size in bytes
+//     size_t sizeInBytes = sizeof(arr[0]) * arr.size();
 
-    cout << "Size of array in bytes: " << sizeInBytes << endl;
+//     cout << "Size of array in bytes: " << sizeInBytes << endl;
 
-    return 0;
-}
+//     return 0;
+// }
   
 
 /*-------------------------------------------------------------------------------------------------------------------------
@@ -31,3 +31,32 @@ It's defined in the <cstddef> or <stddef.h> header.
 --------------------------------------------------------------------------------------------------------------------------*/
 
 
+
+// other way of dynamic allocatio ->
+
+#include <iostream>
+using namespace  std;
+
+int main(){
+    // int *p = new int ;
+    // *p =54;
+    //cout<< *p;
+    // output- 54
+
+    int *p_arr = new int[50] ;// initially 50;
+
+
+
+
+   p_arr [0] =9;
+   p_arr[1]= 20;
+
+   cout<< p_arr<<endl<< *p_arr <<endl<<p_arr[1]<<", for loop contet"<< endl;
+
+   for (int i =0;i<50; ++i ){
+    cout<< endl<<i<<"wow..."<<endl;
+    cout  << p_arr[i];
+    
+   }
+   delete[]p_arr;
+}
