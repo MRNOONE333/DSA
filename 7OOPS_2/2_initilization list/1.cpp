4ebb9e9da2b33978cba7b6320df795ebae0867c3
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2024-06-22 10:13:49 +0530

    day40

---
+++ 7OOPS_2/2_initilization list/1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //int const b;
+    // error bcz no const garbage value  
+
+    // int & j;
+    //error: 'j' declared as reference but not initialized.
+
+
+    //can do
+    // int j=5;
+    // &j;
+    // cout<< &j; 
+
+    // point -1
+    // initilization list -  to initilization constant data 
+    //members , which can't be changed, like we did in deep copy.
+
+    
+
+}
