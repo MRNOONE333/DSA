d0e979cc3fc2ed711f85dc4ebf9d3ae7c57b30e9
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2024-07-22 09:08:32 +0530

    bst  class

---
+++ 13BST/5bstClass/binaryTreeNode.h
@@ -0,0 +1,22 @@
+#ifndef BINARYTREENODE_H
+#define BINARYTREENODE_H
+
+template<typename T>
+class BinaryTreeNode{               
+    public: 
+    T data;
+    BinaryTreeNode *left;
+    BinaryTreeNode *right;// addresses
+
+    BinaryTreeNode(T data){
+        this->data=data;
+        left=NULL;
+        right=NULL ;//values
+    };
+    ~BinaryTreeNode(){
+        delete left;
+        delete right;
+        // todo delete root
+    }
+};
+#endif 
