bffb6cf1496050909b73c5bdda3f44b93ce1df82
haxAbdulAhad <abdul.ahad.21cse@bmu.edu.in>
2024-09-05 20:57:43 +0530

    leetcode 1

---
+++ LEETCODE/1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
+            }
+        }
+        return {};  
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<int> nums = {3,2,4};
+    int target = 6;
+    vector<int> result = sol.twoSum(nums, target);
+
+    if (!result.empty()) {
+        cout << "Indices: " << result[0] << ", " << result[1] << endl;
+    } else {
+        cout << "No solution found!" << endl;
+    }
+
+    return 0;
+}
